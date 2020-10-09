43caea4d261a1a69ab450abb9370f7310011ce70
gour-io <gour-io.github.com>
2020-10-09 10:08:30 +0530

    updating our app with a shop page, collection preview component andd collection item component

--- src/App.js
+++ src/App.js
@@ -2,24 +2,8 @@ import React from 'react';
 import {Switch, Route, Link} from 'react-router-dom';
 import './App.css';
 
-import HomePage from "../src/pages/homepage/homepage.component";
-
-const HatsPage = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <h1>HatsPage</h1>
-    </div>
-  )
-}
-const SneakerPage = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <h1>SneakerPage</h1>
-    </div>
-  )
-}
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
@@ -27,8 +11,7 @@ function App() {
       
       <Switch>
         <Route exact path="/" component={HomePage}/>
-        <Route path="/hats" component={HatsPage}/>
-        <Route path="/sneakers" component={SneakerPage}/>
+        <Route path="/shop" component={ShopPage}/>
       </Switch>
     </div>
   );
